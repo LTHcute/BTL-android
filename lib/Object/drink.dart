1bd5df56a5645b5945fe0a3766c2ebc3a144262e
LTHcute <luongthuhoai01102003@gmail.com>
2024-07-08 21:12:02 +0700

    test

---
+++ lib/Object/drink.dart
@@ -0,0 +1,15 @@
+import 'dart:ffi';
+
+class drink{
+  String sMaDoUong;
+  String sTenDoUong;
+  int iGia;
+  String sThongTinChiTiet;
+  String sSize;
+  int iDuong;
+  int iDa;
+  String sMaTopping;
+  String sImg;
+drink({required this.sMaDoUong,required this.sTenDoUong,required this.iGia, required this.sThongTinChiTiet,required this.sSize, required this.iDuong, required this.iDa, required this.sMaTopping, required this.sImg});
+
+}
